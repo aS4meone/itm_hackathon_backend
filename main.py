7ce905fa978cd1d91abb05d91167451024dc5a6c
Alnur <zholdas.alnur@gmail.com>
2024-09-12 23:59:02 +0500

    feat: gpt added

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI
+import json
+
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+
+from app.schemas.gpt.all import Message, GPTResponse
+from app.utils.gpt import get_response_from_gpt
 
 app = FastAPI()
 
 origins = [
     "*"
 ]
+
+# ПОРТ 8055
 
 app.add_middleware(
     CORSMiddleware,
@@ -16,7 +23,16 @@ app.add_middleware(
 )
 
 
-# ПОРТ 8055
 @app.get("/")
 def root():
     return {"message": "salamaleikum"}
+
+
+@app.post("/chat")
+async def get_gpt_response(message: Message):
+    try:
+        response = get_response_from_gpt(message.message)
+        response_dict = json.loads(response)
+        return GPTResponse(**response_dict)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
